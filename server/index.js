198953c2a7665bffb08f348cba9db639929894fd
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-03-09 22:22:23 +0800

    Start of Express to Koa JS migration:     Started working on rewriting the server code with Koa JS and currently only     wrote in some basic scaffolding example code without route definition for now.     Have only commented out the routes for the Express app and not deleted them,     as will be using them for reference when building the Koa app.

--- server/index.js
+++ server/index.js
@@ -4,36 +4,52 @@
 	Server app instance module for StarterKit's backend service
 */
 
+// // Ping Route to check server status
+// app.get('/ping', (req, res, next) => {
+// 	/*	Things to return to client
+// 		- Status of server
+// 		- Server/Container Load
+// 	*/
+// 	res.end(JSON_string({
+// 		status: 200
+// 	}));
+// });
 
-const express = require('express');
-const app = express();
+// // 404 route handler
+// app.use((req, res, next) => {
+// 	res.status(404).send("ERR 404: Cannot find requested resource");
+// });
+
+// // 500 internal server error route handler
+// app.use((err, req, res, next) => {
+// 	// Log trace stack and error to the console
+// 	error(err.stack);
+// 	res.status(500).send('ERR 500: Internal server error');
+// });
+
+const Koa = require('koa');
+const app = new Koa();
 const { port } = require('./config');
 const { print, error, JSON_string } = require('./utils');
 
-// Create a static server to serve the files in the faq directory
-app.use(express.static('../faqs'));
-
-// Ping Route to check server status
-app.get('/ping', (req, res, next) => {
-	/*	Things to return to client
-		- Status of server
-		- Server/Container Load
-	*/
-	res.end(JSON_string({
-		status: 200
-	}));
+// logger
+app.use(async (ctx, next) => {
+	await next();
+	const rt = ctx.response.get('X-Response-Time');
+	console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 });
 
-// 404 route handler
-app.use((req, res, next) => {
-	res.status(404).send("ERR 404: Cannot find requested resource");
+// x-response-time
+app.use(async (ctx, next) => {
+	const start = Date.now();
+	await next();
+	const ms = Date.now() - start;
+	ctx.set('X-Response-Time', `${ms}ms`);
 });
 
-// 500 internal server error route handler
-app.use((err, req, res, next) => {
-	// Log trace stack and error to the console
-	error(err.stack);
-	res.status(500).send('ERR 500: Internal server error');
+// response
+app.use(async ctx => {
+	ctx.body = 'Hello World';
 });
 
-app.listen(port, () => print(`Server listening to port ${port}`));
+app.listen(3000).on('listening', () => print(`Server listening to port ${port}`));
