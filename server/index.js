c5aa2839932e0917dd08d0fff44c351b52c365c7
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-03-09 21:56:07 +0800

    Cleaned up code for index.js module     Cleaned up the expressjs based server app code inside index.js module with     bare minimum code and routes in it.

--- server/index.js
+++ server/index.js
@@ -1,54 +1,39 @@
 'use strict'; // Enforce use of strict verion of JavaScript
 
 /*	@Doc
-	Main server app instance module.
-	Exports the created server instance out for the routes in the routes folder to access it.
-	This module only holds some misc. routes like ping and error handling routes like for 404.
+	Server app instance module for StarterKit's backend service
 */
+
 
 const express = require('express');
 const app = express();
-module.exports.app = app;
+const { port } = require('./config');
+const { print, error, JSON_string } = require('./utils');
 
-const { print, write, error, JSON_string } = require('./utils');
-const db = require('./db');
-
-/* Global variables */
-const port = 3000;
-
-// Allow express to serve static content to the User from the static assets directory
-app.use(express.static('static'))
+// Create a static server to serve the files in the faq directory
+app.use(express.static('../faqs'));
 
 // Ping Route to check server status
 app.get('/ping', (req, res, next) => {
 	/*	Things to return to client
-		- Number of active connections with socket.io
-		- The number of multiplayer game-rooms alive
-		- Load of the server?
+		- Status of server
+		- Server/Container Load
 	*/
 	res.end(JSON_string({
-		status: 'Server Up',
-	}));
-});
-
-
-1// Route handler for getting the leaderboard
-app.get('/leaderboard', (req, res, next) => {
-	res.end(JSON_string({
-		res: db.get_leaderboard()
+		status: 200
 	}));
 });
 
 // 404 route handler
 app.use((req, res, next) => {
-	res.status(404).send("Sorry can't find that!")
+	res.status(404).send("ERR 404: Cannot find requested resource");
 });
 
 // 500 internal server error route handler
 app.use((err, req, res, next) => {
-	error(err.stack)
-	res.status(500).send('Something broke!')
+	// Log trace stack and error to the console
+	error(err.stack);
+	res.status(500).send('ERR 500: Internal server error');
 });
 
-// Make the HTTP server listen to the port specified by the global variable
-http.listen(port, () => print(`Server listening to port ${port}`));
+app.listen(port, () => print(`Server listening to port ${port}`));
